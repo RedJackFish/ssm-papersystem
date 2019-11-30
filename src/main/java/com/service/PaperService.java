d2c1f12a44bd9121acb65f6ac4668334ff25c274
yuhj <906964494@qq.com>
2019-11-30 20:47:56 +0800

    ssm整合最佳模板 yuhj 20191130 第一次提交

---
+++ src/main/java/com/service/PaperService.java
@@ -0,0 +1,45 @@
+package com.service;
+
+import com.pojo.Paper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface PaperService {
+    /**
+     * 添加论文
+     * @param paper
+     * @return
+     */
+    int addPaper(Paper paper);
+
+    /**
+     * 删除论文
+     * @param id
+     * @return
+     */
+    int deletePaperById(long id);
+
+    /**
+     * 更新修改论文
+     * @param paper
+     * @return
+     */
+    int updatePaper(Paper paper);
+
+    /**
+     * 根据id查找论文
+     * @param id
+     * @return
+     */
+    Paper queryById(long id);
+
+    /**
+     * 查找所有论文
+     * @return
+     */
+    List<Paper> queryAllPaper();
+
+
+}
