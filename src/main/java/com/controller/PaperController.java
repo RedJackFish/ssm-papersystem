b01777665ab1232ec64d90c0516d3aa39483f4a5
yuhj <906964494@qq.com>
2019-12-01 18:42:48 +0800

    20191201 ssm整合是swagger yuhj

--- src/main/java/com/controller/PaperController.java
+++ src/main/java/com/controller/PaperController.java
@@ -2,6 +2,10 @@ package com.controller;
 
 import com.pojo.Paper;
 import com.service.PaperService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +16,12 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/paper")
+@Api(value = "论文接口",tags = "论文增删改查接口")
 public class PaperController {
     @Autowired
     private PaperService paperService;
 
+    @ApiOperation(value="获取用户列表", notes="获取用户列表")
     @RequestMapping("/allPaper")
     public String list(Model model){
         List<Paper> list = paperService.queryAllPaper();
@@ -23,17 +29,22 @@ public class PaperController {
         return "allPaper";
     }
 
+    @ApiOperation(value="添加用户列表页面", notes="添加用户列表页面")
     @RequestMapping("toAddPaper")
     public String toAddPaper(){
         return "addPaper";
     }
 
+    @ApiOperation(value="创建用户" ,notes="创建用户时的注意事项")
+    @ApiImplicitParam(name = "paper", value = "详细实体paper", required = true, dataType = "Paper")
     @RequestMapping("/addPaper")
     public String addPaper(Paper paper){
         paperService.addPaper(paper);
         return "redirect:/paper/allPaper";
     }
 
+    @ApiOperation(value="删除用户" ,notes="根据Id删除用户")
+    @ApiImplicitParam(name = "paperId", value = "详细实体paperId", required = true, dataType = "Int")
     @RequestMapping("/del/{paperId}")
     public String deletePaper(@PathVariable("paperId") Long id){
         paperService.deletePaperById(id);
@@ -46,6 +57,11 @@ public class PaperController {
         return "updatePaper";
     }
 
+    @ApiOperation(value="修改用户" ,notes="修改用户细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "paperId", value = "用户id", required = true, paramType = "path", dataType = "Int"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体Paper", required = true, paramType = "body", dataType = "Paper")
+    })
     @RequestMapping("/updatePaper")
     public String updatePaper(Model model,Paper paper){
         paperService.updatePaper(paper);
